13c7d766a1c3a37b02db72e51488dc603389796c
陈俊宇 <chenjunyu03@meituan.com>
2017-07-14 14:54:30 +0800

    simple doc

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 // make PFC as simple as possible
 module.exports = require('./src');
+
+/**
+ *
+ * @readme-quick-run
+ *
+ * ## test tar=js r_c=pfcCompiler
+ *
+ * let {compile} = pfcCompiler;
+ * let translate = compile('f1(1, 2, g("234", v2))');
+ *
+ * let ret = translate({
+ *   f1: (a, b, c) => a + b + c,
+ *   g: (str1, str2) => Number(str1 + str2),
+ *   v2: "5"
+ * });
+ *
+ * console.log(ret);
+ */
